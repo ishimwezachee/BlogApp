8648982aff80a7201a2f0800afbe64d758f72b63
ishimwezachee <zacheeishimwe@gmail.com>
2022-03-29 19:30:13 +0200

    create controllers and specific routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,5 @@
+class UsersController < ApplicationController
+  def index; end
+
+  def new; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'users/:user_id/posts' => 'posts#index', as: 'user_posts'
+  get 'users/:user_id/posts/:id' => 'posts#show', as: 'user_post'
+  get 'users/' => 'users#index', as: 'users'
+  get 'users/:id' => 'users#show', as: 'user'
 end
