2d08a9c41e441005b442d74a7aa98b375552f3a5
ishimwezachee <zacheeishimwe@gmail.com>
2022-03-30 11:36:27 +0200

    created models and the association for it

---
+++ app/models/comment.rb
@@ -0,0 +1,8 @@
+class Comment < ApplicationRecord
+    belongs_to :author, class_name: 'User'
+    belongs_to :post 
+
+    def update_comment_counter
+        post.update(comment_counter:post.comments.count)
+    end
+end
