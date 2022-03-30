d1098de872f65b7a6546a405c644cd8431dd075f
ishimwezachee <zacheeishimwe@gmail.com>
2022-03-30 19:02:26 +0200

    created a post controller methods and generatd likes controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,4 +9,27 @@ class PostsController < ApplicationController
     @user = @post.author
     @comments = @post.comments
   end
+
+  def new 
+    @post = Post.new
+  end
+
+  def create
+    @user.find(params[:id])
+    @new_post = @user.posts.(post_params)
+    respond_to do |f|
+      f.html do
+        if @new_post.save
+          redirect_to "/users/#{@new_post.author.id}/posts/"
+        else
+          render :new, alert: 'POst failed to create'
+        end
+     end
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+
 end
