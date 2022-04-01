43856884b38b83839e2d32e9bb1ad46fa3ec6ccf
ishimwezachee <zacheeishimwe@gmail.com>
2022-04-01 20:03:46 +0200

    fix linters

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,8 +1,7 @@
 class Api::V1::PostsController < ApplicationController
-    def index
-        @user = User.find(params[:user_id])
-        @posts = @user.posts
-        render json: @posts, status: :ok
-      end
-    
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
+    render json: @posts, status: :ok
+  end
 end
