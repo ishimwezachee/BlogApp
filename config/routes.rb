e656a023f7794c1a2de4b6920d5e622fb12f4a82
ishimwezachee <zacheeishimwe@gmail.com>
2022-04-01 19:50:06 +0200

    Added routing for api

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,20 @@ Rails.application.routes.draw do
     get '/users/sign_out' => 'devise/sessions#destroy' 
   end
   root 'users#index'
+  namespace :api, default: {format: :json} do
+    namespace :v1,  default: {format: :json} do
+      post 'login', to: 'authentication#authenticate'
+      post 'register', to: 'users#create'
+      resources :users do
+        resources :posts, only: [:index, :show, :create] do
+          resources :comments, only: [:index, :create]
+          resources :likes, only: [:create]
+        end
+      end
+    end
+  end
+
+
   resources :users, only: %i[index show] do 
     resources :posts, only: %i[index new create show destroy]
   end
